36405bfbbcfcc8e77387c76b297d6bdc0628286b
Andrea Damiani <andrea_damiani@protonmail.com>
2022-10-03 12:37:40 +0200

    Add mutex to in-mem process store

--- orchestrator/internal/storage/inmem/storage.go
+++ orchestrator/internal/storage/inmem/storage.go
@@ -1,9 +1,14 @@
 package inmem
 
-import "github.com/damianiandrea/go-process-manager/orchestrator/internal/storage"
+import (
+	"sync"
+
+	"github.com/damianiandrea/go-process-manager/orchestrator/internal/storage"
+)
 
 type ProcessStore struct {
 	processesByAgent map[string][]storage.Process
+	mu               sync.RWMutex
 }
 
 func NewProcessStore() *ProcessStore {
@@ -11,14 +16,18 @@ func NewProcessStore() *ProcessStore {
 }
 
 func (s *ProcessStore) GetAll() ([]storage.Process, error) {
+	s.mu.RLock()
 	processes := make([]storage.Process, 0)
 	for _, agentProcesses := range s.processesByAgent {
 		processes = append(processes, agentProcesses...)
 	}
+	s.mu.RUnlock()
 	return processes, nil
 }
 
 func (s *ProcessStore) Put(agentId string, processes []storage.Process) error {
+	s.mu.Lock()
 	s.processesByAgent[agentId] = processes
+	s.mu.Unlock()
 	return nil
 }
